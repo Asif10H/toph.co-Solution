dac0d02aa70e786e226c6b6508211639aa875849
Habibullah Asif <29340163+Habibullah-Asif@users.noreply.github.com>
2019-11-07 21:15:19 +0600

    Update SAtiq The Dengue Fighter.cpp

--- SAtiq The Dengue Fighter.cpp
+++ SAtiq The Dengue Fighter.cpp
@@ -1,1 +1,14 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string str ;
+	getline(cin, str);
+	if(str == "Fever")
+		cout<<"Go to hospital right now.";
+	else
+		cout<<"Go to hospital.";
+	
+}
