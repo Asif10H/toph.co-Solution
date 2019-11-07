ea8fec3f5650b9dfcc74ff7253ed65bdd2f0b085
Habibullah Asif <29340163+Habibullah-Asif@users.noreply.github.com>
2019-11-07 21:18:12 +0600

    Update Alayna's Adventure Journey.cpp

--- Alayna's Adventure Journey.cpp
+++ Alayna's Adventure Journey.cpp
@@ -1,5 +1,3 @@
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,15 +5,13 @@ int main()
 {
    long long int t, a, arr[10001];
    //vector<long long int>arr;
-   cin>>t;
-   for(int i = 1; i <= t; i++)
-   {
        cin>>a;
        for(int m = 0; m < a; m++)
        {
            cin>>arr[m];
        }
+       //sort(arr.begin(), arr.end());
        sort(arr, arr+a, greater<long long int>());
        cout<<arr[0]<<endl;
-   }
+   
 }
